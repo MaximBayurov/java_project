5428c33343d64e5ca4c04ea5021f067fbe90f87c
Maxim Bayurov <vakakak20@gmail.com>
2019-10-03 20:12:49 +0300

    jsoup, новый класс Reader, метод ReadHTML

---
+++ java-graph-app/src/main/java/com/conceptualGraph/Reader.java
@@ -0,0 +1,30 @@
+package com.conceptualGraph;
+
+import org.jsoup.*;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.TextNode;
+
+
+import java.io.*;
+import java.util.List;
+
+public class Reader {
+
+    public static void main(String[] args) {
+        File chosenFile= new File("D:\\Загрузки\\Программирование — Википедия.html");
+        Reader.readHTML(chosenFile);
+    }
+
+    private static void readHTML(File chosenFile) {
+
+        try{
+            PrintWriter writer= new PrintWriter(new BufferedWriter(new FileWriter("filename.out")));
+            Document doc= Jsoup.parse(chosenFile,"UTF-8");
+            writer.print(doc.body().text());
+            writer.flush();
+        } catch (IOException ex){
+            ex.printStackTrace();
+        }
+    }
+
+}
