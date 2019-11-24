4445d753934098714b5618597afea4cab7da256f
Maxim Bayurov <vakakak20@gmail.com>
2019-11-24 16:34:48 +0300

    DictOptimizer new methods

--- java-graph-app/src/main/java/com/conceptualGraph/controller/DictOptimizer.java
+++ java-graph-app/src/main/java/com/conceptualGraph/controller/DictOptimizer.java
@@ -4,10 +4,17 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Scanner;
+import java.util.*;
 
 public class DictOptimizer {
+
+    private HashMap<String, Integer> dictMap;
+
+
+    public DictOptimizer(){
+        dictMap = new HashMap<String, Integer>();
+    }
+
     private String getPage(String pageURL) throws IOException {
         Document doc = Jsoup.connect(pageURL).get();
         String docText = doc.select("" +
@@ -16,16 +23,15 @@ public class DictOptimizer {
         return docText;
     }
 
-    public void makeUpMap(){
-        HashMap<String, Integer> dictMap = new HashMap<String, Integer>();
+    public void makeUpMap(String pageURL){
         try{
-            String pageText = getPage("https://ru.wikipedia.org/wiki/Россия");
+            String pageText = getPage(pageURL);
             Scanner scanner = new Scanner(pageText);
             String word="";
             while (scanner.hasNext()){
                 word=scanner.next();
                 String stemmedWord = Stemmer.stem(WordChecker.bringTo(word));
-                System.out.println(stemmedWord+" | "+word);
+//                System.out.println(stemmedWord+" | "+word);
                 if (!stemmedWord.equals("")){
                     if (!dictMap.containsKey(stemmedWord)){
                         dictMap.put(stemmedWord,1);
@@ -34,10 +40,23 @@ public class DictOptimizer {
                     }
                 }
             }
-            System.out.println(dictMap);
+            System.out.println("Страница: "+pageURL+" прочитана.");
         }catch (IOException ex){
             ex.printStackTrace();
         }
-    };
+    }
+
+    public List sortMap() {
+        List list = new ArrayList(dictMap.entrySet());
+        Collections.sort(list, new Comparator() {
+            @Override
+            public int compare(Object o1, Object o2) {
+                return Integer.parseInt(o2.toString().replaceAll("[^0-9]",""))
+                        -Integer.parseInt(o1.toString().replaceAll("[^0-9]",""));
+            }
+        });
+        System.out.println(list);
+        return list;
+    }
 
 }
